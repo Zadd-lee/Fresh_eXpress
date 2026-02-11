6c70ba7a9a249cb3b8708b8bdc532dec951e9b58
zad <kimminkyeong0418@gmail.com>
2026-02-12 01:54:50 +0900

    feat : user 객체 생성

---
+++ src/main/java/com/mink/freshexpress/user/model/User.java
@@ -0,0 +1,29 @@
+package com.mink.freshexpress.user.model;
+
+import com.mink.freshexpress.common.model.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.Columns;
+
+@Entity
+@Getter
+
+public class User extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String name;
+    private String phone;
+    @Column(nullable = false)
+    private boolean enabled;
+
+}
