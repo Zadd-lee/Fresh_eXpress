3c4ba27fb517385ab8d5818d3c3e6f43e95addb7
zad <kimminkyeong0418@gmail.com>
2026-02-12 01:59:11 +0900

    feat : role 객체 생성
    n:n 관계를 위해 role, userRole 객체 생성

---
+++ src/main/java/com/mink/freshexpress/user/model/Role.java
@@ -0,0 +1,22 @@
+package com.mink.freshexpress.user.model;
+
+import com.mink.freshexpress.common.model.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+    private String description;
+}
---
+++ src/main/java/com/mink/freshexpress/user/model/UserRole.java
@@ -0,0 +1,17 @@
+package com.mink.freshexpress.user.model;
+
+import com.mink.freshexpress.common.model.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Getter
+@Entity
+public class UserRole extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    
+
+}
