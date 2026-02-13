711ca11ad950698cd867279213d6718ad6bd41ae
zad <kimminkyeong0418@gmail.com>
2026-02-13 21:40:19 +0900

    fix : user 생성시 enable 값 default 생성

--- src/main/java/com/mink/freshexpress/user/model/User.java
+++ src/main/java/com/mink/freshexpress/user/model/User.java
@@ -2,9 +2,8 @@ package com.mink.freshexpress.user.model;
 
 import com.mink.freshexpress.common.model.BaseEntity;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.Columns;
 
 import java.util.ArrayList;
@@ -26,7 +25,9 @@ public class User extends BaseEntity {
     @Column(nullable = false)
     private String name;
     private String phone;
+
     @Column(nullable = false)
+    @ColumnDefault("true")
     private boolean enabled;
 
     @OneToMany
