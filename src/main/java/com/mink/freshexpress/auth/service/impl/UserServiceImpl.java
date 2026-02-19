64e169de05cfe1dfb389c7b1d8ea030dd470610e
zad <kimminkyeong0418@gmail.com>
2026-02-19 21:41:53 +0900

    feat : 토큰 재발급 기능 구현 완료

--- src/main/java/com/mink/freshexpress/auth/service/impl/UserServiceImpl.java
+++ src/main/java/com/mink/freshexpress/auth/service/impl/UserServiceImpl.java
@@ -11,14 +11,17 @@ import com.mink.freshexpress.auth.repository.UserRepository;
 import com.mink.freshexpress.auth.service.UserService;
 import com.mink.freshexpress.auth.utils.JwtProvider;
 import com.mink.freshexpress.common.exception.CustomException;
+import com.mink.freshexpress.common.exception.constant.CommonErrorCode;
 import com.mink.freshexpress.common.exception.constant.UserErrorCode;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
@@ -47,7 +50,6 @@ public class UserServiceImpl implements UserService {
 
         userRepository.save(user);
     }
-
     @Override
     public JwtAuthResponseDto login(LoginRequestDto dto) {
 
@@ -67,4 +69,35 @@ public class UserServiceImpl implements UserService {
     }
     @Override
     public JwtAuthResponseDto refreshToken(TokenRequestDto tokenRequestDto) {
+        //1. refresh Token 검증 redis에서 검증
+        if (!jwtProvider.validateToken(tokenRequestDto.getRefreshToken())) {
+            throw new CustomException(CommonErrorCode.UNAUTHORIZED);
+        }
+        
+        //2. Access Token 에서 member id 가져오기
+        Authentication authentication = jwtProvider.getAuthentication(tokenRequestDto.getAccessToken());
+
+
+        // 3. 저장소에서 Member ID 를 기반으로 Refresh Token 값 가져옴
+        RefreshToken refreshToken = refreshTokenRepository.findById(authentication.getName())
+                .orElseThrow(() -> new CustomException(UserErrorCode.NOT_FOUND));
+
+        // 4. Refresh Token 일치하는지 검사
+        if (!refreshToken.getValue().equals(tokenRequestDto.getRefreshToken())) {
+            throw new CustomException(CommonErrorCode.AUTHENTICATE_ERROR);
+        }
+
+        // 5. 새로운 토큰 생성
+        JwtAuthResponseDto jwtAuthResponseDto =  jwtProvider.generateToken(authentication);
+
+        RefreshToken newRefreshToken = new RefreshToken(authentication.getName(), jwtAuthResponseDto.getRefreshToken());
+        refreshTokenRepository.save(newRefreshToken);
+
+
+        // 토큰 발급
+        return jwtAuthResponseDto;
+    }
+
+
+
 }
