f826583629e3285a5625284bbfafdb6a285e5fbe
zad <kimminkyeong0418@gmail.com>
2026-03-11 20:05:09 +0900

    feat : bulk 생성

--- src/main/java/com/mink/freshexpress/stock/service/StockService.java
+++ src/main/java/com/mink/freshexpress/stock/service/StockService.java
@@ -3,6 +3,10 @@ package com.mink.freshexpress.stock.service;
 import com.mink.freshexpress.stock.dto.CreateStockRequestDto;
 import jakarta.validation.Valid;
 
+import java.util.List;
+
 public interface StockService {
     void create(@Valid CreateStockRequestDto dto);
+
+    void creatBulk(@Valid List<CreateStockRequestDto> dtoList);
 }
