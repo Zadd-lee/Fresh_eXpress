7256a81b7d306ab25b4899f0b5d02f957bcc9c92
zad <kimminkyeong0418@gmail.com>
2026-03-12 15:46:57 +0900

    fix : 권한 문제시 출력 에러 해결
    권한 없음 등 문제 시 401,403 에러가 아닌 500 에러가 뜨는 오류 해결

--- src/main/java/com/mink/freshexpress/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/mink/freshexpress/common/exception/GlobalExceptionHandler.java
@@ -3,25 +3,50 @@ package com.mink.freshexpress.common.exception;
 import com.mink.freshexpress.common.ApiResponse;
 import com.mink.freshexpress.common.exception.constant.CommonErrorCode;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
 
 @Slf4j
 @RestControllerAdvice
-public class GlobalExceptionHandler {
-
-    // 커스텀 예외
-    @ExceptionHandler(value = {CustomException.class})
-    public ApiResponse<?> handleCustomException(CustomException e) {
-        log.error("handleCustomException() in GlobalExceptionHandler throw CustomException : {}", e.getMessage());
-        return ApiResponse.fail(e);
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    // custom exception
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ApiResponse<?>> handleCustomException(CustomException e) {
+        log.error("handleCustomException: {}", e.getMessage(), e);
+        return ResponseEntity
+                .status(e.getErrorCode().getHttpStatus())
+                .body(ApiResponse.fail(e));
     }
 
-    // 기본 예외
-    @ExceptionHandler(value = {Exception.class})
-    public ApiResponse<?> handleException(Exception e) {
-        log.error("handleException() in GlobalExceptionHandler throw Exception : {}", e.getMessage());
-        e.printStackTrace();
-        return ApiResponse.fail(new CustomException(CommonErrorCode.INTERNAL_SERVER_ERROR));
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ApiResponse<?>> handleAuthenticationException(AuthenticationException e) {
+        log.error("handleAuthenticationException: {}", e.getMessage(), e);
+        CustomException ex = new CustomException(CommonErrorCode.AUTHENTICATE_ERROR);
+        return ResponseEntity
+                .status(ex.getErrorCode().getHttpStatus())
+                .body(ApiResponse.fail(ex));
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ApiResponse<?>> handleAccessDeniedException(AccessDeniedException e) {
+        log.error("handleAccessDeniedException: {}", e.getMessage(), e);
+        CustomException ex = new CustomException(CommonErrorCode.FORBIDDEN);
+        return ResponseEntity
+                .status(ex.getErrorCode().getHttpStatus())
+                .body(ApiResponse.fail(ex));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<?>> handleException(Exception e) {
+        log.error("handleException: {}", e.getMessage(), e);
+        CustomException ex = new CustomException(CommonErrorCode.INTERNAL_SERVER_ERROR);
+        return ResponseEntity
+                .status(ex.getErrorCode().getHttpStatus())
+                .body(ApiResponse.fail(ex));
     }
 }
