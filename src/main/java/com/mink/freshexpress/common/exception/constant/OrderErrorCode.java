df5af90e6dd702f207662d92a8f728873d9eee33
zad <kimminkyeong0418@gmail.com>
2026-03-18 13:31:08 +0900

    fix : 단건 조회시 error 메세지 수정

--- src/main/java/com/mink/freshexpress/common/exception/constant/OrderErrorCode.java
+++ src/main/java/com/mink/freshexpress/common/exception/constant/OrderErrorCode.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum OrderErrorCode implements ErrorCode{
-    NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 물품입니다.");
+    NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 주문입니다.");
     private final HttpStatus httpStatus;
     private final String message;
 }
